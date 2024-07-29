757e91377ef2fb806ec8419257b3881a5ef81c32
Marcio Fernandes <farciomernandes@gmail.com>
2024-07-29 16:03:50 -0300

    refact: public default route, and delete sh file

--- src/modules/main/main.controller.ts
+++ src/modules/main/main.controller.ts
@@ -1,3 +1,4 @@
+import { Public } from '@modules/auth/decorator/public';
 import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
 import * as process from 'process';
 
@@ -6,6 +7,7 @@ import * as process from 'process';
 })
 export class MainController {
   @Get('')
+  @Public()
   root() {
     const appName = process.env.APP_NAME as string;
     const nodeEnv = process.env.NODE_ENV as string;
