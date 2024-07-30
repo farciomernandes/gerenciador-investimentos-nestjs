12b0d633825c07f9b7f195480974dc7dd197b2e2
Marcio Fernandes <farciomernandes@gmail.com>
2024-07-30 08:23:31 -0300

    feat: add env variables in newrelic

--- newrelic.js
+++ newrelic.js
@@ -9,11 +9,11 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['app'],
+  app_name: [process.env.NEW_RELIC_APP_NAME],
   /**
    * Your New Relic license key.
    */
-  license_key: 'key',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
